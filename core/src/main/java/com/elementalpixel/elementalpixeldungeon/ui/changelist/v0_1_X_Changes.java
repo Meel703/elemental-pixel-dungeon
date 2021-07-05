c7c77b7d26b048143c1785ff73b7d6dc39c664a2
Meel703 <asebo29@gmail.com>
2021-07-05 17:03:58 +0200

    Changed changes

--- core/src/main/java/com/elementalpixel/elementalpixeldungeon/ui/changelist/v0_1_X_Changes.java
+++ core/src/main/java/com/elementalpixel/elementalpixeldungeon/ui/changelist/v0_1_X_Changes.java
@@ -23,8 +23,11 @@ package com.elementalpixel.elementalpixeldungeon.ui.changelist;
 
 
 import com.elementalpixel.elementalpixeldungeon.Assets;
+import com.elementalpixel.elementalpixeldungeon.messages.Messages;
+import com.elementalpixel.elementalpixeldungeon.scenes.ChangesScene;
 import com.elementalpixel.elementalpixeldungeon.sprites.ItemSprite;
 import com.elementalpixel.elementalpixeldungeon.sprites.ItemSpriteSheet;
+import com.elementalpixel.elementalpixeldungeon.ui.Icons;
 import com.elementalpixel.elementalpixeldungeon.ui.Window;
 import com.watabou.noosa.Image;
 
@@ -40,6 +43,7 @@ public class v0_1_X_Changes {
 		
 		add_v0_1_0_Changes(changeInfos);
 		add_v0_1_2_Changes(changeInfos);
+		add_v0_1_2_1_Changes(changeInfos);
 	}
 	
 
@@ -79,5 +83,49 @@ public class v0_1_X_Changes {
 				        "_-_ Goo, DM300 and YogDzewa now drop Elemental Stone which you can use to infuse your armor or weapon, you can infuse both with the same elements (fire, air, water, earth), but they have different effects. Infusion on armor has always defensive effect and infusion on weapon has always offensive effect."
 				)
 		);
+	}
+
+	public static void add_v0_1_2_1_Changes( ArrayList<ChangeInfo> changeInfos) {
+		ChangeInfo changes = new ChangeInfo("v0.1.2.1a", false, "");
+		changes.hardlight(Window.TITLE_COLOR);
+		changeInfos.add(changes);
+
+		changes.addButton(new ChangeButton(new Image(Assets.Sprites.SPINNER, 144, 0, 16, 16), Messages.get(ChangesScene.class, "bugfixes"),
+				"_-_ Fixed many missing descriptions\n\n" +
+						"_-_ Burning on the higher floors doesn't  cause crash anymore\n\n" +
+						"_-_ Elemental stones don't get used if you use them just for info\n\n" +
+						"_-_ Shop on 20th floor is finally working\n\n" +
+						"_-_ Enemies can no longer heal in toxic gas if Elementalist\n"
+				)
+		);
+
+		changes.addButton(new ChangeButton(Icons.get(Icons.PREFS), Messages.get(ChangesScene.class, "misc"),
+				"_-_ Tier 2 Alchemist talent Attuned meal has been finished\n\n" +
+						"_-_ Cost of Alchemist's armor special ability has been changed form 0 to 35\n\n" +
+						"_-_ Icon has been changed\n\n" +
+						"_-_ Dwarf King on floor 50 doesn't drop armor kit anymore\n\n" +
+						"_-_ Water chapter and Air chapter have been swapped\n\n" +
+						"_-_ Grass has been completely removed from Water chapter\n\n" +
+						"_-_ Changed Elemental Stone and Alchemist Dagger sprites\n\n" +
+						"_-_ All heroes start with Dew Vial\n"
+
+				));
+
+		changes.addButton( new ChangeButton(new ItemSprite(ItemSpriteSheet.ELIXIR_TOXIC, null), "Alchemist Flask",
+				"Alchemist Flask has been nerfed:\n\n" +
+						"_-_ Base damage has been lowered from 1-5 to 1-3\n" +
+						"_-_ Alchemist Flask can miss\n" +
+						"_-_ Special effects are more consistent:\n\n" +
+						"_-_ 5% chance to Paralysis\n" +
+						"_-_ 6% chance to Corrosion\n" +
+						"_-_ 6% chance to Root\n" +
+						"_-_ 8% chance to Poison\n" +
+						"_-_ 10% chance to Cripple\n" +
+						"_-_ 15% chance to Burning\n" +
+						"_-_ 15% chance to Frost\n" +
+						"_-_ 15% chance to Toxic Gas\n" +
+						"_-_ 20% chance to no special effect"
+		));
+
 	}
 }
